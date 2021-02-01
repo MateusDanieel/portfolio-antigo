8982ba319566f40788814c446a9d2b36383519b5
Mateus D. Silva <msilva@CORP.TRITONE>
2021-02-01 19:14:41 -0300

    Atualização de stacks e reorganização do projeto.

---
+++ src/js/main.js
@@ -0,0 +1,213 @@
+// TRANSIÇÃO DO MENU
+
+$(window).scroll(function() {
+  
+	if ($(window).scrollTop() > 100 ){
+    
+ 		$('.bg').addClass('down');
+    
+  } else {
+    
+		$('.bg').removeClass('down');
+
+ 	};   	
+});
+
+$('.scroll').on('click', function(e){		
+		e.preventDefault()
+    
+  $('html, body').animate({
+      scrollTop : $(this.hash).offset().top
+    }, 1500);
+});
+
+// SCROLL TO TOP
+
+$(function(){
+	$(document).on( 'scroll', function(){
+		if ($(window).scrollTop() > 100) {
+			$('.smoothscroll-top').addClass('show');
+		} else {
+			$('.smoothscroll-top').removeClass('show');
+		}
+	});
+	$('.smoothscroll-top').on('click', scrollToTop);
+});
+
+function scrollToTop() {
+	var verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+	var element = $('body');
+	var offset = element.offset();
+	var offsetTop = offset.top;
+	$('html, body').animate({scrollTop: offsetTop}, 600, 'linear').animate({scrollTop:25},200).animate({scrollTop:0},150) .animate({scrollTop:0},50);
+}
+
+// HOVER DINAMICO POR SESSÃO
+
+function addClass(el) {
+    menu.removeClass('active');
+    $(el).addClass('active');
+};
+
+var menu = $('.navbar-nav a');
+var sectionPositions = $('section').map(function(){
+    return $(this).position().top;
+});
+var sections = $('section');
+$(document).on('scroll', function () {
+    var scroll = $(document).scrollTop();
+    var currentElement;
+    $(sectionPositions).each(function (i) {
+        if (scroll > this - 100) currentElement = menu[i];
+    });
+    currentElement && addClass(currentElement);
+});
+menu.on('click', function () {
+    addClass(this);
+});
+
+// ROLAGEM SUAVE
+
+$('.navbar-nav a').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 500);
+});
+
+// ROLAGEM SUAVE SETA
+
+$('#pg-header-content a').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 500);
+});
+
+// GALERIA DO PORTFOLIO
+
+let modalId = $('#image-gallery');
+
+$(document)
+  .ready(function () {
+
+    loadGallery(true, 'a.thumbnail');
+
+    function disableButtons(counter_max, counter_current) {
+      $('#show-previous-image, #show-next-image')
+        .show();
+      if (counter_max === counter_current) {
+        $('#show-next-image')
+          .hide();
+      } else if (counter_current === 1) {
+        $('#show-previous-image')
+          .hide();
+      }
+    }
+
+    /**
+     *
+     * @param setIDs        Sets IDs when DOM is loaded. If using a PHP counter, set to false.
+     * @param setClickAttr  Sets the attribute for the click handler.
+     */
+
+    function loadGallery(setIDs, setClickAttr) {
+      let current_image,
+        selector,
+        counter = 0;
+
+      $('#show-next-image, #show-previous-image')
+        .click(function () {
+          if ($(this)
+            .attr('id') === 'show-previous-image') {
+            current_image--;
+          } else {
+            current_image++;
+          }
+
+          selector = $('[data-image-id="' + current_image + '"]');
+          updateGallery(selector);
+        });
+
+      function updateGallery(selector) {
+        let $sel = selector;
+        current_image = $sel.data('image-id');
+        $('#image-gallery-title')
+          .text($sel.data('title'));
+        $('#image-gallery-image')
+          .attr('src', $sel.data('image'));
+        disableButtons(counter, $sel.data('image-id'));
+      }
+
+      if (setIDs == true) {
+        $('[data-image-id]')
+          .each(function () {
+            counter++;
+            $(this)
+              .attr('data-image-id', counter);
+          });
+      }
+      $(setClickAttr)
+        .on('click', function () {
+          updateGallery($(this));
+        });
+    }
+  });
+
+$(document)
+  .keydown(function (e) {
+    switch (e.which) {
+      case 37:
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-previous-image').is(":visible")) {
+          $('#show-previous-image')
+            .click();
+        }
+        break;
+
+      case 39:
+        if ((modalId.data('bs.modal') || {})._isShown && $('#show-next-image').is(":visible")) {
+          $('#show-next-image')
+            .click();
+        }
+        break;
+
+      default:
+        return;
+    }
+    e.preventDefault();
+  });
+
+  // LIGHTZOOM
+
+  $(document).ready(function () {
+    $('img.light-zoom').lightzoom({
+      zoomPower   : 3, 
+      glassSize   : 180, 
+    });
+  });
+
+  // SLICK
+
+  $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+    });
+
+    $('.slider-nav').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    arrows: true,
+    centerMode: true,
+    focusOnSelect: true
+    });
